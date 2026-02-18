4f15b1fee05bfc098e03395882846e1e447044d4
Ahmad Siasa <ahmadsiasa45@gmail.com>
2026-02-18 14:07:09 +0300

    flip back

--- resources/views/patient/chat.blade.php
+++ resources/views/patient/chat.blade.php
@@ -344,7 +344,7 @@
                                             onkeyup="checkTyping()" onkeydown="if(event.key === 'Enter' && !event.shiftKey) { event.preventDefault(); sendMessage(); }">
 
                                         <div class="form-buttons">
-                                            <button class="btn send-btn" type="submit" id="sendMessageBtn" onclick="sendMessage()">
+                                            <button class="btn send-btn" type="button" id="sendMessageBtn" onclick="sendMessage()">
                                                 <i class="isax isax-send-25"></i>
                                             </button>
                                         </div>
