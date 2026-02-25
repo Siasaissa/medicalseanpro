890ad7cdb861d5a729d7922101c7e29f86196f2e
Ahmad Siasa <ahmadsiasa45@gmail.com>
2026-02-25 22:00:20 +0300

    one more

--- resources/views/layouts/doctorHeader.blade.php
+++ resources/views/layouts/doctorHeader.blade.php
@@ -33,18 +33,16 @@
 									<li class="has-submenu">
 										<a href="#">Blog Grid</i></a>
 									</li>
-									<li>
-									<a href="{{ route('logout') }}" 
-										class="btn btn-md btn-primary-gradient d-inline-flex align-items-center rounded-pill"
-										onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-										<i class="isax isax-lock-1 me-1"></i> Logout
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-
-								</li>
+									<li class="menu">
+										<a href="#">
+										<form method="POST" action="{{ route('logout') }}">
+											@csrf
+											<button type="submit" style="background:none;border:none;padding:0;color:inherit;cursor:pointer;">
+												Logout
+											</button>
+										</form>
+										</a>
+									</li>
 									
 									
 								</ul>
