506e5ce92539d302338cd8928b8d6d20b6b9fb70
Ahmad Siasa <ahmadsiasa45@gmail.com>
2026-02-25 14:07:24 +0300

    yes

--- resources/views/admin/appointment.blade.php
+++ resources/views/admin/appointment.blade.php
@@ -59,8 +59,8 @@
 													<td>{{ $appo->doctor->profile->speciality }}</td>
 													<td>
 														<h2 class="table-avatar">
-															<a href="profile.html" class="avatar avatar-sm me-2"><img class="avatar-img rounded-circle" src="{{asset($appo->user->profile->dp) ?? ''}}" alt="User Image"></a>
-															<a href="profile.html">{{ $appo->patient->name }} </a>
+															<a href="profile.html" class="avatar avatar-sm me-2"><img class="avatar-img rounded-circle" src="{{asset($appo->patient->profile->dp) ?? ''}}" alt="User Image"></a>
+															<a href="profile.html">{{ $appo->patient->name }} {{ dd($appo->user_id, $appo->patient) }}</a>
 														</h2>
 													</td>
 													<td> <span class="text-primary d-block">{{ $appo->appointment_datetime }}</span></td>
