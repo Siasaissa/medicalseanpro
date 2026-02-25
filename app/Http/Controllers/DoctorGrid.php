1666123cdfca233c5ef215564d9ef61aba299802
Ahmad Siasa <ahmadsiasa45@gmail.com>
2026-02-25 12:46:40 +0300

    for ending time

--- app/Http/Controllers/DoctorGrid.php
+++ app/Http/Controllers/DoctorGrid.php
@@ -18,7 +18,8 @@ public function grid(Request $request)
     {
         $query = User::whereHas('profile', function($q) {
             $q->whereNotNull('id') // Ensure user has a profile
-                ->where('start_time', '>', Carbon::now());
+                ->where('start_time', '>', Carbon::now())
+                ->where('end_time', '<', Carbon::now());
         })  ->where('role', 'doctor');
 
             
