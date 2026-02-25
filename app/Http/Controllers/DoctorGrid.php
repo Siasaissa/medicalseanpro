38dd62d30b6a6004f513e580421c798f7cca5d4e
Ahmad Siasa <ahmadsiasa45@gmail.com>
2026-02-25 13:20:23 +0300

    one more

--- app/Http/Controllers/DoctorGrid.php
+++ app/Http/Controllers/DoctorGrid.php
@@ -16,8 +16,8 @@ class DoctorGrid extends Controller
 {
 public function grid(Request $request)
     {
-        dd(now(), now()->format('H:i:s'));
-        
+        dd(Profile::select('start_time','end_time')->get());
+
         $query = User::whereHas('profile', function($q) {
             $q->whereNotNull('id') // Ensure user has a profile
                 ->where('start_time', '<=', Carbon::now()->format('H:i:s'))
