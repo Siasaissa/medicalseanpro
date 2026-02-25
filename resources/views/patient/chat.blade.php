34cdafb6c24186e266b3478558677e36aa2d1140
Ahmad Siasa <ahmadsiasa45@gmail.com>
2026-02-25 20:44:04 +0300

    one more

--- resources/views/patient/chat.blade.php
+++ resources/views/patient/chat.blade.php
@@ -142,12 +142,12 @@
                                             <figure class="avatar {{ $activeDoctor?->is_online ? 'avatar-online' : 'avatar-offline' }}">
                                                 <img src="{{ $activeDoctorImage }}" alt="{{ $activeDoctor?->name ?? 'Doctor' }}" id="currentDoctorImage" style="border-radius: 50%;">
                                             </figure>
-                                            <div class="mt-1">
+                                            <div class="mt-1" style="z-index: 100">
                                                 <h5 id="currentDoctorName">{{ $activeDoctor?->name ?? 'Select a chat' }}</h5>
                                                 <small class="last-seen" id="doctorStatus">
                                                     @if($activeDoctor)
                                                         <span class="online-status {{ $activeDoctor->is_online ? 'text-success' : 'text-secondary' }}">
-                                                            {{ $activeDoctor->is_online ? '● Online' : '○ Offline dope' }}
+                                                            {{ $activeDoctor->is_online ? '● Online' : '○ Offline' }}
                                                         </span>
                                                     @else
                                                         &nbsp;
