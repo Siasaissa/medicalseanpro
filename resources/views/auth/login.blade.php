bc84b357e006f87b9b5ce033ccc418b482af1ec3
Ahmad Siasa <ahmadsiasa45@gmail.com>
2026-02-26 15:08:34 +0300

    one more

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -131,10 +131,10 @@
 												<span class="span-or">or</span>
 											</div>
 											<div class="social-login-btn">
-												<a href="{{ route('google.login') }}" class="btn w-100">
+												<a href="javascript:void(0);" class="btn w-100">
 													<img src="images/google-icon.svg" alt="google-icon">Sign in With Google
 												</a>
-												<a href="#" class="btn w-100">
+												<a href="javascript:void(0);" class="btn w-100">
 													<img src="images/facebook-icon.svg" alt="fb-icon">Sign in With Facebook
 												</a>
 											</div>
