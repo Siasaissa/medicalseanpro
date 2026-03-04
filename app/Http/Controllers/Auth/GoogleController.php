ad6f8adbb9d7092fd123330af4a3df603f0d50cb
Ahmad Siasa <ahmadsiasa45@gmail.com>
2026-03-04 15:08:14 +0300

    one more

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -13,7 +13,7 @@ class GoogleController extends Controller
     public function redirectToGooglePatient()
     {
         session(['google_role' => 'patient']);
-        $redirectUrl = config('services.google.redirect') . '/patient';
+        $redirectUrl = config('services.google.redirect') . '';
         return Socialite::driver('google')
             ->redirectUrl($redirectUrl)
             ->redirect();
