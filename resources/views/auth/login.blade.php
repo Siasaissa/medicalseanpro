18cdd5463bba95e3beb55805e52efe4ef8c8011f
Ahmad Siasa <ahmadsiasa45@gmail.com>
2026-03-04 16:14:54 +0300

    little test

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -132,7 +132,7 @@
 											</div>
 											<div class="social-login-btn">
 												<a href="{{ route('google.patient') }}" class="btn w-100">
-													<img src="images/google-icon.svg" alt="google-icon">Sign In With Google for patient
+													<img src="images/google-icon.svg" alt="google-icon">Sign In for patient
 												</a>
 												<!--<a href="javascript:void(0);" class="btn w-100">
 													<img src="images/facebook-icon.svg" alt="fb-icon">Sign in With Facebook
